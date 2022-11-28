1e3881436c34c3f55ae2cf470a60944c15d9c547
jvtian <kevinjwtian@gmail.com>
2022-11-27 22:21:06 -0500

    Implementing ViewPage basic function

---
+++ src/pages/ViewPage.js
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import {Button,Card, Drawer, Input} from 'antd';
+import axios from 'axios';
+import {createRef, useEffect, useState} from 'react';
+import {v4 as uuidv4} from 'uuid';
+
+export const ViewPage = () => {
+  return (<>
+
+  </>);
+};
