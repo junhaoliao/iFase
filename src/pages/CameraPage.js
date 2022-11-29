5c0996151846f24384d698787897a1d9c01ca8d3
shirley <shirley.qin@mail.utoronto.ca>
2022-11-28 19:16:59 -0500

    added the capture pic and able to access index

--- src/pages/CameraPage.js
+++ src/pages/CameraPage.js
@@ -1,7 +1,7 @@
-/* the following code opens a webcam of on a separate page
 import * as React from "react";
 import Webcam from "react-webcam";
 import * as ReactDOM from 'react-dom';
+
 const videoConstraints = {
   width: 1280,
   height: 720,
@@ -9,51 +9,50 @@ const videoConstraints = {
 };
 
 export const CameraPage = () =>{
-    ReactDOM.render(
-    <Webcam />,
-    document.getElementById('root') // uses index.html
-    );
-}*/
+    // ReactDOM.render(
+    // <Webcam />,
+    // document.getElementById('root')
+    // );
+  //   ReactDOM.render(
+  //   <Webcam
+  //   audio={false}
+  //   height={720}
+  //   screenshotFormat="image/jpeg"
+  //   width={1280}
+  //   videoConstraints={videoConstraints}
+  // >
+  //   {({ getScreenshot }) => (
+  //     <button
+  //       onClick={() => {
+  //         const imageSrc = getScreenshot()
+  //       }}
+  //     >
+  //       Capture photo
+  //     </button>
+  //   )}
+  // </Webcam>,
+  //   document.getElementById('root')
+  //   );
 
-import React, { useState, useEffect } from 'react';
-
-export const CameraPage = () => {
-  const [stream, setStream] = useState(null);
-
-  useEffect(() => {
-    if (!stream) return;
-    const video = document.querySelector('video');
-    video.srcObject = stream;
-  }, [stream]);
-
-  const handleStart = () => {
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
-      .then(s => setStream(s))
-      .catch(err => console.error(err));
-  };
-
-  const handleStop = () => {
-    setStream(null);
-  };
-
+    const webcamRef = React.useRef(null);
+      const capture = React.useCallback(
+        () => {
+          const imageSrc = webcamRef.current.getScreenshot();
+        },
+        [webcamRef]
+      );
   return (
-    <div>
-      <div style={{ width: '1280px', height: '480px' }}>
-        {stream ? (
-          <video style={{ width: '100%', height: '100%' }} autoPlay playsInline muted />
-        ) : (
-          <p>Your Web Camera is Off</p>
-        )}
-      </div>
-      <div>
-        {stream ? (
-          <button onClick={handleStop}>Stop</button>
-        ) : (
-          <button onClick={handleStart}>Start</button>
-        )}
-      </div>
-    </div>
+    <>
+      <Webcam
+        audio={false}
+        height={720}
+        ref={webcamRef}
+        screenshotFormat="image/jpeg"
+        width={1280}
+        videoConstraints={videoConstraints}
+      />
+      <button onClick={capture}>Capture photo</button>
+    </>
   );
-};
 
-
+}
