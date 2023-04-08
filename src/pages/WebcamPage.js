6456543f78a80e0394597583fffa53a92ef6904b
jvtian <kevinjwtian@gmail.com>
2023-04-07 23:50:22 -0400

    update requirement.txt

--- src/pages/WebcamPage.js
+++ src/pages/WebcamPage.js
@@ -21,8 +21,6 @@ import * as React from "react";
 
 
 export const WebcamPage = () => {
-  const [testContent, setTestContent] = useState("");
-  const [isPaused, setIsPaused] = useState(false);
 
   let newWindow;
 
@@ -34,33 +32,14 @@ export const WebcamPage = () => {
     axios.get('http://127.0.0.1:5000/stream_pause')
       .then((resp) => {
         console.log(resp);
-        setIsPaused(prevState => !prevState);
       })
   };
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (!isPaused) {
-        axios.get('http://127.0.0.1:5000/test_func')
-          .then((resp) => {
-            setTestContent(resp.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      }
-    }, 1000);
-    return () => clearInterval(interval);
-  }, [isPaused]);
 
   return (
     <div>
       <Button variant="contained" onClick={webcam}>Start Recognizing</Button>
-      <Button variant="contained" onClick={stop}>{isPaused ? "Resume" : "Pause"}</Button>
+      <Button variant="contained" onClick={stop}>Pause/Resume</Button>
       <br />
-      <Card>
-        <p>{testContent}</p>
-      </Card>
     </div>
   );
 
