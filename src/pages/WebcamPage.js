ce45d740a747a96b347185272a9d4d6425e38335
jvtian <kevinjwtian@gmail.com>
2023-04-08 01:11:21 -0400

    Display realtime recognition natively in webcam page

--- src/pages/WebcamPage.js
+++ src/pages/WebcamPage.js
@@ -12,8 +12,9 @@
                     console.log(resp)
                 })
         }
-*/
 
+*/
+/*
 import { Button, Card, Drawer, Input } from 'antd';
 import axios from 'axios';
 import { useRef, useEffect, useState } from 'react';
@@ -43,4 +44,69 @@ export const WebcamPage = () => {
     </div>
   );
 
+};
+
+*/
+
+/*
+STABLE using iframe
+import { Button, Card, Drawer, Input } from 'antd';
+import axios from 'axios';
+import { useRef, useEffect, useState } from 'react';
+import * as React from "react";
+
+
+export const WebcamPage = () => {
+  const [testContent, setTestContent] = useState("");
+  const [isPaused, setIsPaused] = useState(false);
+
+  const handleStartCaptureClick = () => {
+    const iframe = document.createElement('iframe');
+    iframe.src = 'http://127.0.0.1:5000/test_func';
+    iframe.width = '100%';
+    iframe.height = '500px';
+    document.querySelector('.camera-container').appendChild(iframe);
+  };
+
+
+  return (
+    <div>
+      <div className="camera-container"></div>
+      <Button variant="contained" onClick={handleStartCaptureClick}>Start Recognizing</Button>
+
+      <br />
+      <Card>
+        <p>{testContent}</p>
+      </Card>
+    </div>
+  );
+
+};
+ */
+
+import { Button, Card, Drawer, Input } from 'antd';
+import axios from 'axios';
+import { useRef, useEffect, useState } from 'react';
+import * as React from "react";
+
+export const WebcamPage = () => {
+  const [isPaused, setIsPaused] = useState(false);
+
+  const handleTogglePause = () => {
+    setIsPaused((prevState) => !prevState);
+    axios.get('http://127.0.0.1:5000/stream_pause')
+      .then((resp) => {
+        console.log(resp);
+      })
+  };
+
+  return (
+    <div>
+      <img src={`http://127.0.0.1:5000/test_func`} width="888" height="500"/>
+      <br />
+      <Button onClick={handleTogglePause}>
+        {isPaused ? 'Resume' : 'Pause'}
+      </Button>
+    </div>
+  );
 };
